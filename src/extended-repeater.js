abddff663cc8519a4b56009f6a7a3e7e039e2a81
Alina <alinarafaelevna@mail.ru>
2023-12-25 03:11:42 +0300

    fix: 1-3, 8, 19

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -10,16 +10,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * @example
  * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
+function repeater(str, options) {
+  
+  let num = options.repeatTimes === undefined ? 1 : options.repeatTimes,
+    sep = options.separator === undefined  ? "+" : String(options.separator),
+    add = options.addition === undefined ?  "" : options.addition === null ? "null" : options.addition.toString(),
+    addSep = options.additionSeparator === undefined ? "|" : String(options.additionSeparator),
+    addRepTimes = options.additionRepeatTimes === undefined? 1 : options.additionRepeatTimes;
+  if (options.addition === null )
+    add = 'null';
+  else if (typeof options.addition === 'object' && options.addition[Symbol.toPrimitive] != undefined)
+    add = String(options.addition[Symbol.toPrimitive]('string'));
+  let prom =  Array(addRepTimes).fill(add).join(addSep);
+  return Array(num).fill(str+prom).join(sep);
+  
   // remove line with error and write your code here
 }
-
 module.exports = {
   repeater
 };
